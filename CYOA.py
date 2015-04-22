aeee5385d80200b00a60e7cf10da7426f06c26d2
Morgan3828 <morgan5091@gmail.com>
2015-04-22 10:22:33 -0700

    Worked on Morgan's code

--- CYOA.py
+++ CYOA.py
@@ -18,37 +18,75 @@ w.pack()
 def begin():
     """First Decision"""
     # User Info then first choice
-    messagebox.showinfo("Hello!", "This is the beginning of your story!")
+    messagebox.showinfo("Hello!", "This is the beginning of YOUR story!")
     firstchoice = simpledialog.askinteger("First choice:",
-                                        "Your walking home, and there are 2 paths.",
-                                        "Which do you take?",
-                                        "1 for the shady alley",
-                                        "2 for (TO BE DECIDED)")
+                                        "Your walking home, and there are 2 paths."+
+                                        " Which do you take?"+
+                                        " 1 for the shady alley"+
+                                        ", 2 for the sidewalk")
     if firstchoice == 1:
         secondchoiceA()
+        secondchoiceAR()
     elif firstchoice == 2:
         secondchoiceB()
+        secondchoiceBR()
     else:
         begin()
 
 def secondchoiceA():
+    """Second decision"""
+    # User Info then second choice
     messagebox.showinfo("Story Explaination",
-                        "You chose the Shady Alley way."
-                        ,"In that case this'll be fun.")
+                        "You chose the Shady Alley way."+
+                        " In that case this'll be fun.")
     messagebox.showinfo("Story Explaination",
-                        "You walk down the alley and you see a",
-                        " blue box with a sign on top labeled",
-                        " 'POLICE PUBLIC CALL BOX'.",
-                        "",
-                        "Will you enter the Police box, or",
-                        "continue to walk elsewhere?")
+                        "You walk down the alley and you see a"+
+                        " blue box with a sign on top labeled"+
+                        " 'POLICE PUBLIC CALL BOX'."+
+                        " Will you enter the Police box, or"+
+                        " continue to walk elsewhere?")
+def secondchoiceAR():
     
+    secondchoice = simpledialog.askinteger("Story Decision",
+                            "1 to enter the box"+
+                            ", 2 to go elsewhere:")
+    if secondchoice == 1:
+        thirdchoiceA()
+        thirdchoiceAR()
+    elif secondchoice == 2:
+        thirdchoiceB()
+        thirdchoiceBR()
+    else:
+        secondchoiceAR()
+
 
 
 #########################  Jacob  #########################
 
+def secondchoiceB():
+    """Second decision"""
+    # User Info then third choice
+    messagebox.showinfo("Story Explaination",
+                        "You chose the sidewalk, "+
+                        "like a rational, sane person would ")
+    messagebox.showinfo("Story Explaination",
+                        "")
 
-
+def secondchoiceBR():
+    
+    secondchoice = simpledialog.askinteger("Story Decision",
+                            "1 to enter the box"+
+                            ", 2 to go elsewhere:")
+    if secondchoice == 1:
+        thirdchoiceA()
+        thirdchoiceAR()
+    elif secondchoice == 2:
+        thirdchoiceB()
+        thirdchoiceBR()
+    else:
+        secondchoiceAR()
+# Note for Jacob: the "R" next to the "A & B" are for a restart setting.
+# It's for in the case that the user chooses another number other than 1 or 2
 
 ####################  HundredVisions Guy  #################
 def intro():
@@ -67,13 +105,7 @@ def intro():
     else:
         intro()
 
-
-
-
-
-
-
-########################   Main Code 1  #####################
+########################   Code Start  #####################
 begin()
 
 
